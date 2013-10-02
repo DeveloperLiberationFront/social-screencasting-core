f31a040e3aefd26feedc4f4fcbb3898148eb4d6d
Kevin Lubick <kjlubick@ncsu.edu>
2013-10-02 15:00:32 -0400

    FindBugs: Bug: Hard coded reference to an absolute pathname in org.lubick.unitTests.TestVideoPostProduction.test()

---
+++ org/lubick/unitTests/TestVideoPostProduction.java
@@ -0,0 +1,20 @@
+package org.lubick.unitTests;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class TestVideoPostProduction {
+
+	@Test
+	public void test() {
+		
+		File capFile = new File("/src/ForTesting/oneMinuteCap.cap");
+		assertTrue(capFile.exists());
+		
+		fail("Not yet implemented");
+	}
+
+}
