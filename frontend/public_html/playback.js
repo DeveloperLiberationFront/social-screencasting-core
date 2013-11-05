9c9153a7bbe7ea9b6a812d13bdbef6db1cfe2818
Kevin Lubick <kjlubick@ncsu.edu>
2013-11-05 13:31:08 -0500

    Basic animation working.

--- frontend/public_html/playback.js
+++ frontend/public_html/playback.js
@@ -1,4 +1,4 @@
-
+var isPlaying = false;
 
 function launchFullScreen(element) {  //From davidwalsh.name/fullscreen
   if(element.requestFullScreen) {
@@ -10,13 +10,55 @@ function launchFullScreen(element) {  //From davidwalsh.name/fullscreen
   }
 }
 
+/*function goFullScreenAndStartPlaying()
+{
+	$(".animation").first().addClass("visible");
+	launchFullScreen($("#panel")[0]);
+	
+	var totalFrames = $(".animation").length;
+	var currentFrame = 0;
+	window.setInterval(function(){
+		$(".animation").eq(currentFrame).removeClass("visible");
+		currentFrame = (currentFrame + 1) % totalFrames;
+		$(".animation").eq(currentFrame).addClass("visible");
+		document.webkitFullscreenElement = $(".animation").eq(currentFrame)[0];
+	},500);
+}*/
+
 function goFullScreenAndStartPlaying()
 {
-	launchFullScreen(document.getElementById("panel"));
+	launchFullScreen($("#panel")[0]);
+	
+	if (!isPlaying)
+		startAnimation();
 }
-
+function startAnimation()
+{
+	isPlaying = true;
+	var totalFrames = $(".animation").length;
+	var currentFrame = 0;
+	window.setInterval(function(){
+		$(".animation").eq(currentFrame).hide();
+		currentFrame = (currentFrame + 1) % totalFrames;
+		$(".animation").eq(currentFrame).show();
+	},500);
+}
 
 $(document).ready(function()
 {
+	$(".animation").first().show();
 	$("#experimental").on("click", goFullScreenAndStartPlaying);
+	
+	preloadImages();
+	
+	
 });
+
+function preloadImages()
+{
+	var p = $("#panel");
+	$('<img class="animation" src="bug8/temp0001.png"></img>').appendTo(p);
+	$('<img class="animation" src="bug8/temp0002.png"></img>').appendTo(p);
+	$('<img class="animation" src="bug8/temp0003.png"></img>').appendTo(p);
+	$('<img class="animation" src="bug8/temp0004.png"></img>').appendTo(p);
+}
