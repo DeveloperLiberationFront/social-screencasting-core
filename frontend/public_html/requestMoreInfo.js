c0a2bcabd44954995dc19b86bcc41024083d1054
Kevin Lubick <kjlubick@ncsu.edu>
2013-11-06 14:10:55 -0500

    Started pulling playback code out as template

--- frontend/public_html/requestMoreInfo.js
+++ frontend/public_html/requestMoreInfo.js
@@ -1,49 +1,5 @@
-$(document).ready(function () {
-    //handles the first click to see if a video file exists
-    $(".clickMe").on('click', function () {
-        $(".moreInfo").removeClass("hidden");
-        $.post("makeVideo", {
-            thingToDo: "isVideoAlreadyMade",
-            pluginName: $(this).data("pluginName"),
-            toolName: $(this).data("toolName")
-        },
 
-		handleVideoFileExists);
-    });
-
-    $(".clickMe").on('mouseenter', function () {
-        $(this).addClass("hoverOver");
-    });
-    $(".clickMe").on('mouseleave', function () {
-        $(this).removeClass("hoverOver");
-    });
-
-    //handles the "second" click to generate said video file
-
-
-});
-
-function handleVideoFileExists(data) {
-    $(".modal").hide();
-    $(".moreInfo").html($(data).hide());
-
-    $(".moreInfo").children().fadeIn("fast");
-    addRequestGenerationListeners();
-
-}
-//To be called to dynamically allow jquery nodes to be ex
-function addRequestGenerationListeners() {
-    $(".requestGeneration").on('click', function () {
-        $(".moreInfo").removeClass("hidden");
-        $(".modal").show();
-        $.post("makeVideo", {
-            thingToDo: "makeVideo",
-            pluginName: $(this).data("pluginName"),
-            toolName: $(this).data("toolName")
-        },
-            handleVideoFileExists);
-    });
-
+function setUpFancyBox() {
     $('.fancybox-thumbs').fancybox({
         prevEffect: 'none',
         nextEffect: 'none',
@@ -60,3 +16,56 @@ function addRequestGenerationListeners() {
         }
     });
 }
+
+//To be called to dynamically allow jquery nodes to be patched into
+/*function addRequestGenerationListeners() {
+    $(".requestGeneration").on('click', requestGenerationOfMedia);
+
+    setUpFancyBox();
+}*/
+
+function addResponseHTMLToWindow(data) {
+    $(".modal").hide();
+    $(".moreInfo").html($(data).hide());
+
+    $(".moreInfo").children().fadeIn("fast");
+    //addRequestGenerationListeners();
+    setUpFancyBox();
+
+}
+
+function requestGenerationOfMedia() {
+    $(".moreInfo").removeClass("hidden");
+    $(".modal").show();
+    $.post("makeVideo", {
+        thingToDo: "makeVideo",
+        pluginName: $(this).data("pluginName"),
+        toolName: $(this).data("toolName")
+    }, addResponseHTMLToWindow);
+    //this will return the html to view the media
+}
+
+function doesVideoExistHuh() {
+    $(".moreInfo").removeClass("hidden");
+    $.post("makeVideo", {
+        thingToDo: "isVideoAlreadyMade",
+        pluginName: $(this).data("pluginName"),
+        toolName: $(this).data("toolName")
+    }, addResponseHTMLToWindow);
+    //This will either return the html to view the media, 
+    //or some html prompting the user to make the media
+}
+
+$(document).ready(function () {
+    //handles the click on the view buttons to see if a video file exists
+    $(".clickMe").on('click', doesVideoExistHuh);
+
+    $(".clickMe").on('mouseenter', function () {
+        $(this).addClass("hoverOver");
+    });
+    $(".clickMe").on('mouseleave', function () {
+        $(this).removeClass("hoverOver");
+    });
+
+    $(".moreInfo").on('click', '.requestGeneration', requestGenerationOfMedia);
+});
