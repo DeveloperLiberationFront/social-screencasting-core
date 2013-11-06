16fc3e50f9b19f9ed7ecdf7f56d91370dcb27021
Kevin Lubick <kjlubick@ncsu.edu>
2013-11-06 08:32:02 -0500

    Used JSHint on requestMoreInfo.js

--- frontend/public_html/requestMoreInfo.js
+++ frontend/public_html/requestMoreInfo.js
@@ -1,55 +1,62 @@
-$(document).ready(function()
-{
-//handles the first click to see if a video file exists
-	$(".clickMe").on('click',function(){
-		$(".moreInfo").removeClass("hidden");
-		$.post( "makeVideo", { 
-		thingToDo: "isVideoAlreadyMade", pluginName: $(this).data("pluginName"), toolName:$(this).data("toolName")},
+$(document).ready(function () {
+    //handles the first click to see if a video file exists
+    $(".clickMe").on('click', function () {
+        $(".moreInfo").removeClass("hidden");
+        $.post("makeVideo", {
+            thingToDo: "isVideoAlreadyMade",
+            pluginName: $(this).data("pluginName"),
+            toolName: $(this).data("toolName")
+        },
 
 		handleVideoFileExists);
-	});
-	
-	$(".clickMe").on('mouseenter',function(){
-		$(this).addClass("hoverOver");
-	});
-	$(".clickMe").on('mouseleave',function(){
-		$(this).removeClass("hoverOver");
-	});
+    });
 
-//handles the "second" click to generate said video file
-	
+    $(".clickMe").on('mouseenter', function () {
+        $(this).addClass("hoverOver");
+    });
+    $(".clickMe").on('mouseleave', function () {
+        $(this).removeClass("hoverOver");
+    });
+
+    //handles the "second" click to generate said video file
+
 
 });
 
-function handleVideoFileExists( data ) {
-	$(".modal").hide();
-  $(".moreInfo").html( $(data).hide() );
-  
-  $(".moreInfo").children().fadeIn("fast");
-  addRequestGenerationListeners();
-  
+function handleVideoFileExists(data) {
+    $(".modal").hide();
+    $(".moreInfo").html($(data).hide());
+
+    $(".moreInfo").children().fadeIn("fast");
+    addRequestGenerationListeners();
+
 }
 //To be called to dynamically allow jquery nodes to be ex
-function addRequestGenerationListeners(){
-	$(".requestGeneration").on('click',function(){
-		$(".moreInfo").removeClass("hidden");
-		$(".modal").show();
-		$.post( "makeVideo", { thingToDo: "makeVideo", pluginName: $(this).data("pluginName"), toolName:$(this).data("toolName")}, handleVideoFileExists);
-	});
-	
-	$('.fancybox-thumbs').fancybox({
-				prevEffect : 'none',
-				nextEffect : 'none',
+function addRequestGenerationListeners() {
+    $(".requestGeneration").on('click', function () {
+        $(".moreInfo").removeClass("hidden");
+        $(".modal").show();
+        $.post("makeVideo", {
+            thingToDo: "makeVideo",
+            pluginName: $(this).data("pluginName"),
+            toolName: $(this).data("toolName")
+        },
+            handleVideoFileExists);
+    });
 
-				closeBtn  : false,
-				arrows    : false,
-				nextClick : true,
+    $('.fancybox-thumbs').fancybox({
+        prevEffect: 'none',
+        nextEffect: 'none',
 
-				helpers : {
-					thumbs : {
-						width  : 50,
-						height : 50
-					}
-				}
-			});
+        closeBtn: false,
+        arrows: false,
+        nextClick: true,
+
+        helpers: {
+            thumbs: {
+                width: 50,
+                height: 50
+            }
+        }
+    });
 }
