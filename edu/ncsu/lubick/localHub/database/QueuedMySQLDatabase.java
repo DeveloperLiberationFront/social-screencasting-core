0daab6e2bba836486a0c85b5c138f04e7f37ed9a
Kevin Lubick <kjlubick@ncsu.edu>
2013-12-30 14:28:19 -0600

    FindBugs: Bug: Null pointer dereference of QueuedMySQLDatabase.connection in edu.ncsu.lubick.localHub.database.QueuedMySQLDatabase.checkDatabaseConnection()

--- edu/ncsu/lubick/localHub/database/QueuedMySQLDatabase.java
+++ edu/ncsu/lubick/localHub/database/QueuedMySQLDatabase.java
@@ -12,7 +12,7 @@ public class QueuedMySQLDatabase extends SQLDatabase {
 
 	private static Logger logger = Logger.getLogger(QueuedMySQLDatabase.class.getName());
 	private Connection connection;
-	
+
 	public QueuedMySQLDatabase()
 	{
 		try
@@ -25,13 +25,13 @@ public class QueuedMySQLDatabase extends SQLDatabase {
 			throw new DBAbstractionException("Could not find driver for MySQLDatabase", e);
 		}
 		openRemoteConnection();
-		}
+	}
 
 	private void loadDatabaseDriver() throws ClassNotFoundException
 	{
 		Class.forName("com.mysql.jdbc.Driver");
 	}
-	
+
 	private void openRemoteConnection()
 	{
 		if (this.connection != null)
@@ -41,13 +41,13 @@ public class QueuedMySQLDatabase extends SQLDatabase {
 		Connection newConnection = null;
 		try
 		{
-			 newConnection = DriverManager.getConnection("jdbc:mysql://eb2-2291-fas01.csc.ncsu.edu:4747/screencast?user=screencast_user&password=screencast");
+			newConnection = DriverManager.getConnection("jdbc:mysql://eb2-2291-fas01.csc.ncsu.edu:4747/screencast?user=screencast_user&password=screencast");
 		}
 		catch (SQLException e)
 		{
 			logger.error("Problem connecting to MySQLDatabase", e);
 		}
-		
+
 		if (newConnection != null)
 		{
 			this.connection = newConnection;
@@ -58,28 +58,61 @@ public class QueuedMySQLDatabase extends SQLDatabase {
 	@Override
 	public void close()
 	{
-		// TODO Auto-generated method stub
-
+		if (connection != null)
+		{
+			try
+			{
+				connection.close();
+			}
+			catch (SQLException e)
+			{
+				throw new DBAbstractionException(e);
+			}
+		}
 	}
-	
+
 	@Override
 	protected void createTables()
 	{
-		//we shouldn't make the tables in the MySQL because the are remotely managed
+		//we shouldn't make the tables in the MySQL because they are remotely managed
 	}
 
 	@Override
 	protected PreparedStatement makePreparedStatement(String statementQuery)
 	{
+		if (checkDatabaseConnection())
+		{
+			try
+			{
+				return connection.prepareStatement(statementQuery);
+			}
+			catch (SQLException e)
+			{
+				throw new DBAbstractionException(e);
+			}
+		}
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	private boolean checkDatabaseConnection()
+	{
+		try
+		{
+			return connection != null || connection.isValid(1);
+		}
+		catch (SQLException e)
+		{
+			logger.error("Problem validating connection", e);
+			return false;
+		}
 	}
 
 	@Override
 	protected void executeStatementWithNoResults(PreparedStatement statement)
 	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
