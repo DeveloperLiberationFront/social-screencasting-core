5c3a9140dbee4673155c00c6d34212d38dac0486
Kevin Lubick <kjlubick@ncsu.edu>
2014-03-04 17:14:34 -0500

    Cleaned up leftovers

--- edu/ncsu/lubick/localHub/http/HTTPMediaResourceHandler.java
+++ edu/ncsu/lubick/localHub/http/HTTPMediaResourceHandler.java
@@ -2,10 +2,7 @@ package edu.ncsu.lubick.localHub.http;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -20,34 +17,22 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import edu.ncsu.lubick.localHub.WebQueryInterface;
-import edu.ncsu.lubick.localHub.videoPostProduction.PostProductionHandler;
-import freemarker.template.SimpleNumber;
-import freemarker.template.SimpleScalar;
-import freemarker.template.TemplateHashModel;
-import freemarker.template.TemplateModel;
-import freemarker.template.TemplateModelException;
 
 public class HTTPMediaResourceHandler extends TemplateHandlerWithDatabaseLink implements Handler {
 
 	private static final String CLIP_NAME = "clipName";
 	private static final String PERFORM_ACTION = "thingToDo";
-	private static final String POST_COMMAND_NTH_USAGE = "nthUsage";
+
 	private static final String POST_COMMAND_PLUGIN_NAME = "pluginName";
-	//private static final String POST_COMMAND_IS_VIDEO_MADE_FOR_TOOL_USAGE = "isVideoAlreadyMade";
 
 	private static final String POST_COMMAND_TOOL_NAME = "toolName";
 	
 	private static final String QUERY_CLIP_EXISTANCE = "queryClipExistance";
 	private static final String GET_IMAGES_FOR_CLIP = "getImages";
 	
-	//private static final String POST_COMMAND_VIEW_NTH_USAGE = "changeToOtherSource";
-	//private static final String POST_COMMAND_MAKE_PLAYBACK_HTML_FOR_EXTERNAL_CLIP = "makePlaybackForExternalClip";
-	
+
 	private static Logger logger;
 	
-	private static final File MEDIA_OUTPUT_FOLDER = new File(PostProductionHandler.MEDIA_OUTPUT_FOLDER);
-	
-
 	// static initializer
 	static
 	{
@@ -160,8 +145,7 @@ public class HTTPMediaResourceHandler extends TemplateHandlerWithDatabaseLink im
 		
 		try
 		{
-			jarr.put("Eclipsedc3a37d4-4469-391f-bd62-0324ac2b7091");
-			//clips.put("clips", jarr);
+			clips.put("clips", jarr);
 			clips.put("clips",jarr);
 			
 			response.setContentType("application/json");
@@ -177,163 +161,13 @@ public class HTTPMediaResourceHandler extends TemplateHandlerWithDatabaseLink im
 
 	}
 
-	private void serveUpNthUsageOfExternalMedia(HttpServletResponse response, String toolName, String pluginName, Integer nthUsage,
-			JSONArray jarrOfClipNames)
-	{
-		// TODO Auto-generated method stub
-		
-	}
-
-	private List<File> getFoldersPrefixedWith(String folderName)
-	{
-		File[] outputMediaTypes = MEDIA_OUTPUT_FOLDER.listFiles();
-		Arrays.sort(outputMediaTypes, new Comparator<File>() {
-
-			@Override
-			public int compare(File o1, File o2)
-			{
-				return o2.getName().compareTo(o1.getName());		//sort it backwards so that the bigger numbers (most recent dates)
-																	//come first
-			}
-		});
-		logger.debug("checking to see if "+Arrays.toString(outputMediaTypes)+" starts with "+folderName);
-		List<File> files = new ArrayList<>();
-		for(File f: outputMediaTypes)
-		{
-			if (f.isDirectory() && f.getPath().startsWith(folderName))
-			{
-				files.add(f);
-			}
-		}
-		return files;
-	}
-
-	private int countNumFrames(File mediaDir)
-	{
-		int count = 0;
-		String[] fileNames = mediaDir.list();
-		for (String fileName : fileNames)
-		{
-			if (fileName.startsWith("frame"))
-			{
-				count++;
-			}
-		}
-		return count;
-	}
-	
-	private void processTemplateWithNameKeysAndNumFrames(HttpServletResponse response, String keypress, InternalToolRepresentation itr, int numFrames, int totalMediaOptions) throws IOException
-	{
-		HashMap<Object, Object> templateData = new HashMap<Object, Object>();
-
-		templateData.put("playbackDirectory", itr.directory);
-		templateData.put("toolName", itr.humanToolName);
-		templateData.put("pluginName", itr.humanPluginName);
-		templateData.put("keypress", keypress);
-		templateData.put("totalFrames", numFrames);
-
-		if (totalMediaOptions > 1)	//if we only have 1 option, don't give more options
-		{
-			List<DisplayOtherMediaOption> otherOptions = makeMoreOptions(itr.nthMostRecent, totalMediaOptions);
-
-			templateData.put("viewMoreOptions", otherOptions);
-		}
-		
-		processTemplate(response, templateData, "playback.html.piece");
-	}
-
-	public List<DisplayOtherMediaOption> makeMoreOptions(int currentlySelected, int totalMediaOptions)
-	{
-		List<DisplayOtherMediaOption> otherOptions = new ArrayList<>();
-		otherOptions.add(new DisplayOtherMediaOption(currentlySelected == 0, 0, "Most Recent"));
-		if (totalMediaOptions > 1)
-		{
-			otherOptions.add(new DisplayOtherMediaOption(currentlySelected == 1 ,1, "2nd Most Recent"));
-		}
-		if (totalMediaOptions > 2)
-		{
-			otherOptions.add(new DisplayOtherMediaOption(currentlySelected == 2 ,2, "3rd Most Recent"));
-		}
-		if (totalMediaOptions > 3)
-		{
-			for (int i = 3;i<totalMediaOptions;i++)
-			{
-				otherOptions.add(new DisplayOtherMediaOption(currentlySelected == i ,i, ""+(i+1)+"th"));
-			}
-		}
-		return otherOptions;
-	}
-	
-
-
 	@Override
 	protected Logger getLogger()
 	{
 		return logger;
 	}
 
-	private class DisplayOtherMediaOption implements TemplateHashModel
-	{
-		boolean isActivated;
-		int number;
-		String text;
 
-
-		public DisplayOtherMediaOption(boolean isActivated, int number, String text)
-		{
-			this.isActivated = isActivated;
-			this.number = number;
-			this.text = text;
-		}
-
-		@Override
-		public TemplateModel get(String queryItem) throws TemplateModelException
-		{
-			switch (queryItem)
-			{
-			case "additionalClass":
-				if (isActivated)
-				{
-					return new SimpleScalar("activated");
-				}
-				return new SimpleScalar("");
-			case "number":
-				return new SimpleNumber(number);
-			case "text":
-				return new SimpleScalar(text);
-			}
-			return null;
-		}
-
-		@Override
-		public boolean isEmpty() throws TemplateModelException
-		{
-			return false;
-		}
-
-	}
 	
-	private class InternalToolRepresentation
-	{
-		
-		private String humanToolName;
-		private int nthMostRecent;
-		private String directory;
-		private String humanPluginName;
-
-		public InternalToolRepresentation(String toolName, String pluginName, String directory, int nthMostRecent)
-		{
-			this.humanToolName = toolName;
-			this.nthMostRecent = nthMostRecent;
-			this.directory = directory;
-			this.humanPluginName = pluginName;
-		}
-		
-		@Override
-		public String toString()
-		{
-			return "The "+nthMostRecent+"th most recent usage of "+humanToolName;
-		}
-	}
 
 }
