624160d81a33d0be416d0f2f334db1869005cc60
Kevin Lubick <kjlubick@ncsu.edu>
2014-03-09 19:59:33 -0400

    Cleaned out old files for more consistent tests

--- edu/ncsu/lubick/unitTests/TestClipCreationLimits.java
+++ edu/ncsu/lubick/unitTests/TestClipCreationLimits.java
@@ -17,6 +17,7 @@ import edu.ncsu.lubick.localHub.ToolStream.ToolUsage;
 import edu.ncsu.lubick.localHub.forTesting.IdealizedToolStream;
 import edu.ncsu.lubick.localHub.forTesting.LocalHubDebugAccess;
 import edu.ncsu.lubick.localHub.forTesting.TestingUtils;
+import edu.ncsu.lubick.localHub.videoPostProduction.PostProductionHandler;
 import edu.ncsu.lubick.util.FileUtilities;
 
 public class TestClipCreationLimits {
@@ -25,7 +26,7 @@ public class TestClipCreationLimits {
 	private static final File renderedVideos = new File("renderedVideos/");
 	private static final int MAX_TOOL_USAGES = LocalHub.MAX_TOOL_USAGES;
 	private LocalHubDebugAccess server;
-	
+
 	private static final Logger logger = Logger.getLogger(TestClipCreationLimits.class);
 
 
@@ -33,11 +34,35 @@ public class TestClipCreationLimits {
 	public static void setUpClass() throws Exception
 	{
 		TestingUtils.makeSureLoggingIsSetUp();
-		
+
 		//TODO unzip if needed
 		File dir = new File(TEST_SCREENCASTING_DIR);
 		assertTrue(dir.isDirectory());
 		assertTrue(dir.list().length > 10);
+
+		clearOutAllTestingMedia();
+	}
+
+
+	private static void clearOutAllTestingMedia()
+	{
+		String[] list = renderedVideos.list();
+		for(String packageName : list)
+		{
+			if (packageName.startsWith(TestPostProductionHandler.TEST_PLUGIN_NAME))
+			{
+				File expectedDir = new File(renderedVideos, packageName);
+				logger.debug("clearing out and deleting "+expectedDir);
+				if (expectedDir.exists())
+				{
+					assertTrue(TestingUtils.clearOutDirectory(expectedDir));
+					assertTrue(expectedDir.delete());
+				}
+				else {
+					logger.error(packageName+" really should exist... ["+expectedDir+"]");
+				}
+			}
+		}
 	}
 
 
@@ -48,42 +73,39 @@ public class TestClipCreationLimits {
 		this.server = LocalHub.startTESTINGServerAndReturnDebugAccess(TEST_SCREENCASTING_DIR);
 		assertNotNull(server);
 		assertTrue(server.isRunning());
-		
+
 		ToolStream toolStreamOfFourTools = makeToolStreamOfFour();
 		ToolStream toolStreamOfTwoTools = makeToolStreamOfMaxPlusOne();
 		ToolStream excellentExample = makeSuperiorExample();
-		
-		clearOutAlreadyCreatedMedia(toolStreamOfFourTools);
-		clearOutAlreadyCreatedMedia(toolStreamOfTwoTools);
-		clearOutAlreadyCreatedMedia(excellentExample);
-		
+
+
 		int presize = renderedVideos.list().length;
-		
+
 		server.reportToolStream(toolStreamOfFourTools);
-		
+
 		int midSize = renderedVideos.list().length;
-		
+
 		assertEquals(4, midSize - presize);
 		//fail();
-		
+
 		server.reportToolStream(toolStreamOfTwoTools);
-		
+
 		int postSize = renderedVideos.list().length;
-		
+
 		assertEquals(MAX_TOOL_USAGES, postSize - presize);
-		
+
 		//First 5 instances will have been made, the sixth ignored
 		List<String> verifiedFiles = checkFileNamesForFirstFive(toolStreamOfFourTools, toolStreamOfTwoTools);
-		
-		
-		
+
+
+
 		server.reportToolStream(excellentExample);
-		
+
 		String[] postList = renderedVideos.list();
 		postSize = postList.length;
-		
+
 		assertEquals(MAX_TOOL_USAGES, postSize - presize);
-		
+
 		//the last one (the shortest duration) should get swapped for 
 		verifiedFiles.set(MAX_TOOL_USAGES-1, getFolderNameForToolUsage(excellentExample.getAsList().get(0)));
 	}
@@ -91,24 +113,24 @@ public class TestClipCreationLimits {
 
 	private List<String> checkFileNamesForFirstFive(ToolStream toolStreamOfFourTools, ToolStream toolStreamOfTwoTools)
 	{
-		
+
 		List<String> expectedToolUsageFiles = new ArrayList<>();
-		
+
 		for(ToolUsage tu : toolStreamOfFourTools.getAsList())
 		{
 			String truncatedName = getFolderNameForToolUsage(tu);
 			expectedToolUsageFiles.add(truncatedName);
 		}
-		
+
 		for(int i = expectedToolUsageFiles.size();i<MAX_TOOL_USAGES;i++)
 		{
 			ToolUsage tu = toolStreamOfTwoTools.getAsList().get(i-4);
 			String truncatedName = getFolderNameForToolUsage(tu);
 			expectedToolUsageFiles.add(truncatedName);
 		}
-		
+
 		checkRenderedFilesForExistanceOfNames(expectedToolUsageFiles);
-		
+
 		return expectedToolUsageFiles;
 	}
 
@@ -123,7 +145,7 @@ public class TestClipCreationLimits {
 			{
 				logger.debug("found created clip: "+fileName);
 				assertTrue("Expecting to find "+fileName, expectedNames.contains(fileName));
-				
+
 				numHits++;
 			}
 		}
@@ -157,7 +179,7 @@ public class TestClipCreationLimits {
 		{
 			iToolStream.addToolUsage("WhomboTool #5", "", "Ctrl+5", new Date(62_000L + 2000*i), 1500 - i);
 		}
-		
+
 		ToolStream convertedToolStream = ToolStream.generateFromJSON(iToolStream.toJSON());
 		convertedToolStream.setAssociatedPlugin(TestPostProductionHandler.TEST_PLUGIN_NAME);
 		return convertedToolStream;
@@ -173,7 +195,7 @@ public class TestClipCreationLimits {
 		{
 			iToolStream.addToolUsage("WhomboTool #5", "", "Ctrl+5", new Date(62_000L + 2000*i), 1500 - i);
 		}
-		
+
 		ToolStream convertedToolStream = ToolStream.generateFromJSON(iToolStream.toJSON());
 		convertedToolStream.setAssociatedPlugin(TestPostProductionHandler.TEST_PLUGIN_NAME);
 		return convertedToolStream;
