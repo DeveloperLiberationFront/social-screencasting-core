75df80bab54790dde011b5a77a3684239930ba0b
Samuel Christie <schrist@ncsu.edu>
2014-06-24 17:46:29 -0400

    Working keyboard overlay

--- frontend/public_html/app/controllers.js
+++ frontend/public_html/app/controllers.js
@@ -116,19 +116,35 @@ define(['angular',
   .controller('PlayerCtrl', ['$scope', '$interval',
     function($scope, $interval) {
         $scope.player = {
-            frames: ["frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg","frame000.jpg"],
+            frames: ["frame0000.jpg","frame0001.jpg","frame0002.jpg","frame0003.jpg","frame0004.jpg","frame0005.jpg","frame0006.jpg","frame0007.jpg","frame0008.jpg","frame0009.jpg","frame0010.jpg","frame0011.jpg","frame0012.jpg","frame0013.jpg","frame0014.jpg","frame0015.jpg","frame0016.jpg","frame0017.jpg","frame0018.jpg","frame0019.jpg","frame0020.jpg","frame0021.jpg","frame0022.jpg","frame0023.jpg","frame0024.jpg","frame0025.jpg","frame0026.jpg","frame0027.jpg","frame0028.jpg","frame0029.jpg","frame0030.jpg","frame0031.jpg","frame0032.jpg","frame0033.jpg","frame0034.jpg","frame0035.jpg","frame0036.jpg","frame0037.jpg","frame0038.jpg","frame0039.jpg","frame0040.jpg","frame0041.jpg","frame0042.jpg","frame0043.jpg","frame0044.jpg"],
             pos: 0,
-            frameCount: 100,
+            frameCount: 45,
             playing: false
+            
         };
         $scope.isFullscreen = false;
-        $scope.imgDir = 'images/';
+        $scope.imgDir = 'images/Eclipse0f989895-65a6-3af3-bf02-497cd0f4f35bK/';
         $scope.toolName = 'test';
         $scope.showRating = false;
         $scope.playBtnImages = {
             true: 'images/playback/pause.svg',
             false:'images/playback/play.svg'
         }
+        $scope.keyboardMode = 0;
+        $scope.keyboardOverlay = 'inactive';
+        $scope.keyboardImages = {
+            'active': [ 'image_text.png', 'image.png', 'text.png' ],
+            'inactive': [ 'image_text_un.png', 'image_un.png', 'text_un.png']
+        }
+        $scope.clipDetails = {
+            keyboardEventFrame: 25,
+        }
+
+        $scope.$watch(function(){ 
+            return $scope.player.pos > $scope.clipDetails.keyboardEventFrame; 
+        }, function(newVal, oldVal) {
+            $scope.keyboardOverlay = (newVal ? 'active' : 'inactive')
+        });
 
         $scope.timer = $interval(function() {
             if ($scope.player.playing) { //playing
