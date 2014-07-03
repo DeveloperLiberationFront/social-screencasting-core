2b70f48a51a702d24b17f853d10e6e5ba2650b6f
Kevin Lubick <kjlubick@ncsu.edu>
2014-07-03 11:29:40 -0400

    Making fake requests

--- edu/ncsu/lubick/externalAPI/ExternalShareClipRequester.java
+++ edu/ncsu/lubick/externalAPI/ExternalShareClipRequester.java
@@ -97,15 +97,15 @@ public class ExternalShareClipRequester {
 
 
 	
-	private static void main(String[] args) throws Exception
+	public static void main(String[] args) throws Exception
 	{
 		TestingUtils.makeSureLoggingIsSetUp();
-		UserManager userManager = new UnitTestUserManager("Test User", "test@mailinator.com", "123");
-		
+		//UserManager userManager = new UnitTestUserManager("Test User", "test@mailinator.com", "123");
+		UserManager userManager = new UnitTestUserManager("Kevin Test", "kjlubick+test@ncsu.edu", "221ed3d8-6a09-4967-91b6-482783ec5313");
 		
 		ExternalShareClipRequester sharer = new ExternalShareClipRequester(userManager);
 		
-		sharer.requestClipsFromUser("kjlubick@ncsu.edu", "Eclipse", "Organize Imports");
+		sharer.requestClipsFromUser("test@mailinator.com", "Eclipse", "Organize Imports");
 		
 	}
 
