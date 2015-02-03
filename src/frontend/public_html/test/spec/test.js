4422f4e6eb8c590c6a526856b7979f2d9be9ebd8
anlawande <devnull@localhost>
2015-02-03 14:20:06 -0500

    Removed commented code

--- src/frontend/public_html/test/spec/test.js
+++ src/frontend/public_html/test/spec/test.js
@@ -7,39 +7,9 @@ define(
   function( angular, mocks ){
     var http;
     
-//    beforeEach(function(){
-//      angular.module('e2e-mocks', ['ngMockE2E'])
-//      .run(function($httpBackend) {
-//          // Do your mock
-//          var baseApiUrl = '/mock';
-//          var axelStudent = {
-//              Education: [{}],
-//              Person: {}
-//          };
-//          var femaleStudent = {
-//              Education: [{}],
-//              Person: {}
-//          };
-//          $httpBackend.whenGET(baseApiUrl + 'students/?searchString=axe&amp;')
-//              .respond([axelStudent, femaleStudent]);
-//
-//          // Don't mock the html views
-//          $httpBackend.whenGET(/views\/\w+.*/).passThrough();
-//
-//          $httpBackend.whenGET('/api/user').respond({"user":{"token":"221ed3d8-6a09-4967-91b6-482783ec5313","email":"kjlubick+test@ncsu.edu","name":"Kevin Test"}});
-//          $httpBackend.whenGET('http://recommender.oscar.ncsu.edu/api/v2/applications').respond({"_items": [{"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Excel", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6c", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6c", "_etag": "2c9ad2c2422b63e2f1fadc5444e09b9de69006c6"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Eclipse", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6d", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6d", "_etag": "4366baa39d445db4a8eb15b5824ed7e230a553e7"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "[ScreencastingHub]", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6e", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6e", "_etag": "e4e3edbf71d630dec55040ec4f5d740264377878"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Gmail", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6f", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6f", "_etag": "46e7afc9fb6b51cf85d8fc104a5b0bf8d3b1e893"}], "_links": {"self": {"href": "/applications", "title": "applications"}, "parent": {"href": "", "title": "home"}}});
-//          // For everything else, don't mock
-//          $httpBackend.whenGET(/.*/).passThrough();
-//          $httpBackend.whenPOST(/^\w+.*/).passThrough();
-//        });
-//
-//    });
     beforeEach(mocks.module(function($provide) {
       $provide.constant('User' , {"token":"221ed3d8-6a09-4967-91b6-482783ec5313","email":"kjlubick+test@ncsu.edu","name":"Kevin Test"});
     }));
-    beforeEach(mocks.module('socasterApp', [function(){
-//    	angular.module('socasterApp').requires.push('e2e-mocks');
-    }]));
 
     describe('Controller', function() {
       var $controller;
@@ -68,10 +38,6 @@ define(
         expect($scope.user.email).toEqual('kjlubick+test@ncsu.edu');
       });
       
-      /*it('Application filter', function() {
-        var $scope = scope;
-        var controller = $controller('RootCtrl', { $scope: $scope});
-      });*/
     });
 
     describe("Services", function() {
@@ -99,14 +65,13 @@ define(
       });
       
       it("should test getting applications", function() {
-          http.whenGET('http://recommender.oscar.ncsu.edu/api/v2/applications').respond({"_items": [{"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Excel", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6c", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6c", "_etag": "2c9ad2c2422b63e2f1fadc5444e09b9de69006c6"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Eclipse", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6d", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6d", "_etag": "4366baa39d445db4a8eb15b5824ed7e230a553e7"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "[ScreencastingHub]", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6e", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6e", "_etag": "e4e3edbf71d630dec55040ec4f5d740264377878"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Gmail", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6f", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6f", "_etag": "46e7afc9fb6b51cf85d8fc104a5b0bf8d3b1e893"}], "_links": {"self": {"href": "/applications", "title": "applications"}, "parent": {"href": "", "title": "home"}}});
-          var user = hub.all('applications').getList().then(function(data) {
-            expect(_.any(data, {name: 'Excel'})).toEqual(true);
-            //expect(data.user.email).toEqual('kjlubick+test@ncsu.edu');
-          });
-
-          http.flush();
+        http.whenGET('http://recommender.oscar.ncsu.edu/api/v2/applications').respond({"_items": [{"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Excel", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6c", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6c", "_etag": "2c9ad2c2422b63e2f1fadc5444e09b9de69006c6"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Eclipse", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6d", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6d", "_etag": "4366baa39d445db4a8eb15b5824ed7e230a553e7"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "[ScreencastingHub]", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6e", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6e", "_etag": "e4e3edbf71d630dec55040ec4f5d740264377878"}, {"_updated": "Thu, 01 Jan 1970 00:00:00 GMT", "name": "Gmail", "_links": {"self": {"href": "/applications/5453d90b0d1ff13d518c3b6f", "title": "Application"}}, "_created": "Thu, 01 Jan 1970 00:00:00 GMT", "_id": "5453d90b0d1ff13d518c3b6f", "_etag": "46e7afc9fb6b51cf85d8fc104a5b0bf8d3b1e893"}], "_links": {"self": {"href": "/applications", "title": "applications"}, "parent": {"href": "", "title": "home"}}});
+        var user = hub.all('applications').getList().then(function(data) {
+          expect(_.any(data, {name: 'Excel'})).toEqual(true);
         });
+
+        http.flush();
+      });
     });
   }
 );
